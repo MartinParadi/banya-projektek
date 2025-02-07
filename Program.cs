d7930beef2728c1f16e96db971151b083d4a7f39
MartinParadi <paradi.martin@students.jedlik.eu>
2025-02-07 08:44:48 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace banya_projekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
